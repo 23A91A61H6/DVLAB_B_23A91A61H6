f3b7ab3e9c1cea149f30d38f9cbc7aad1678e4df
unknown <rajasanvy2@gmail.com>
2026-02-28 11:51:53 +0530

    week1

---
+++ scripts/week1(a).R
@@ -0,0 +1,77 @@
+# load the dataset
+
+data(iris)
+?iris
+
+
+###  Inspect the data
+
+head(iris)
+str(iris)
+View(iris)
+
+class(iris)
+
+# VERY BASIC BAR CHAT 
+# Count of each specie  
+
+#LABELED bar chart
+
+barplot(
+  table(iris$Species),
+  main = "Count of Iris Species",
+  xlab = "Species",
+  ylab = "No of flowers"
+)
+
+# BAR CHART FROM AGGREGATED DATA (Base R)
+
+#Mean Sepal Length Per Species
+mean_sepal <- tapply(iris$Sepal.Length, iris$Species, mean)
+mean_sepal
+
+barplot(
+  mean_sepal,
+  col = 'violet',
+  main = "Average Sepal Length by Species",
+  ylab = "Mean Sepal Length"
+)
+
+
+# GROUPED(Clustered ) BAR CHART( Base R)
+
+group_means <- rbind(
+  Sepal = tapply(iris$Sepal.Length, iris$Species, mean),
+  Petal = tapply(iris$Petal.Length, iris$Species, mean)
+)
+
+group_means
+
+group_means1 <- rbind(
+  Sepal_length = tapply(iris$Sepal.Length, iris$Species, mean),
+  Petal_length = tapply(iris$Petal.Length, iris$Species, mean),
+  Sepal_width = tapply(iris$Sepal.Width, iris$Species, mean),
+  Petal_width = tapply(iris$Petal.Width, iris$Species, mean)
+)
+
+group_means1
+
+barplot(
+  group_means,
+  beside = TRUE,
+  col = c("red","orange"),
+  legend.text = TRUE,
+  main = 'Grouped Bar Chart: SEPAL VS PETAL length'
+)
+
+
+
+# STACKED BAR CHART( bASE r)
+
+barplot(
+  group_means1,
+  beside = FALSE,
+  col = c('pink','black','blue','green'),
+  legend.text = TRUE,
+  main = "Stacked Bar Chart: Sepal Vs Petal Length"
+)
